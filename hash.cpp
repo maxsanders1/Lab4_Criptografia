4971b9034097a550af76d44041e3cc4cceccb9ab
MaxSanders-work <82481063+MaxSanders-work@users.noreply.github.com>
2021-10-14 04:01:58 -0300

    Add files via upload

--- hash.cpp
+++ hash.cpp
@@ -1,31 +1,106 @@
 #include <iostream>
 #include <fstream>
 #include <math.h>
+#include <regex>
+#include <ctime>
+
 using namespace std;
-/* ---------------------------------- HASH (Pendiente) ----------------------------------- */
-/*
-    input
-    separar el input en n/2 arreglos de tamaño 2 (Que hacer con impares?)
-    Convertirlo en bits o hecadecimal?
-    en caso de bits XOR
-    en caso de hexadecimal, sumar un hexadecimal estatico?
-    concatenar resultado
-*/
-string hasheador(string s){
-    return s;
+/* ---------------------------------- funciones utiles ----------------------------------- */
+long long convertBinary(int n){
+    long long bin = 0;
+    int rem, i = 1;
+    while (n!=0) {
+        rem = n % 2;
+        n /= 2;
+        bin += rem * i;
+        i *= 10;
+    }
+    return bin;
+}
+
+int XORfunction(int x){
+    string b = to_string(x);
+    string xorpilar = "0101010";
+    string aux = "";
+    for (int i = 0; i < b.length() ; i++){
+        if (xorpilar[i] != b[i]){
+            aux = aux + "1";
+        }
+        else{
+            aux = aux + "0";
+        }
+    }
+    int result = 0;
+    result = stoi(aux);
+    return result;
+}
+
+long fromBin(long n){
+    long factor = 1;
+    long total = 0;
+    while (n != 0){
+        total += (n%10) * factor;
+        n /= 10;
+        factor *= 2;
+    }
+    return total;
+}
+
+/* ---------------------------------- HASH ----------------------------------- */
+string hasheador(string key){
+    //recibir palabra
+    int size = key.length();
+    int arrNumb[size];
+
+    //pasarlos a numero
+    for (int i = 0; i < size; i++){
+        arrNumb[i] = (int)key[i];
+    }
+
+    //pasarlo a binario
+    for (int i = 0; i < size; i++){
+        arrNumb[i] = convertBinary(arrNumb[i]);
+    }
+
+    //realizar XOR con cada variable
+    for (int  i = 0; i < size; i++){
+        arrNumb[i] = XORfunction(arrNumb[i]);
+    }
+
+    //devolverlo a decimal
+    for (int  i = 0; i < size; i++){
+        arrNumb[i] = fromBin(arrNumb[i]);
+    }
+
+    //pasar a ASCII
+    char r[size];
+    for (int  i = 0; i < size; i++){
+        r[i] = (char)arrNumb[i];
+    }
+
+    //pasar a string
+    string result = "";
+    for (int  i = 0; i < size; i++){
+        result = result + r[i];
+    }
+
+    return result;
 }
 /* --------------------------------------------------------------------------- */
 
-/* ---------------------------------- Comprobacion de Largo (Pendiente) ----------------------------------- */
+/* ---------------------------------- Comprobacion de Largo ----------------------------------- */
 string lengthComp(string s){
     // relleno en caso de largo minimo no alcanzado
-    if (s.length() < 10){
-        int ceros = 10 - s.length();
-        string relleno = "";
-        for (int i = 0; i < ceros; i++){
-            relleno = relleno + "0";
+    time_t now = time(0);
+    tm *ltm = localtime(&now);
+    int year = 1900 + ltm->tm_year;
+    int month = 1 + ltm->tm_mon;
+    if (s.length() < 25){
+        while (s.length() < 25){
+            if (s.length() < 25){
+                s = s + to_string(year) + to_string(month);
+            }
         }
-        s = s + relleno;
         return s;
     }
     else{
@@ -34,77 +109,101 @@ string lengthComp(string s){
 }
 /* ------------------------------------------------------------------------------------------------------- */
 
-/* ----------------------------------Calcular Entropia CHECK----------------------------------------- */
-int entropy(string s){
-    int l = s.length();
-    int w = 1; // Como identificar base?
-    int r = l * log2(w);
+/* ----------------------------------Calcular Entropia ----------------------------------------- */
+float entropy(string s){
+    float l = s.length();
+    float w = 0;
+    regex numbers("([0-9])");
+    if (regex_search(s, numbers)){
+        w = w + 10;
+    }
+    regex lower("([a-z])");
+    if (regex_search(s, lower)){
+        w = w + 26;
+    }
+    regex upper("([A-Z])");
+    if (regex_search(s, upper)){
+        w = w + 26;
+    }
+    regex signals("([`~!@#$%^&*()_=+{}|;:',<.>/?ñÑ])");
+    if (regex_search(s, upper)){
+        w = w + 29;
+    }
+    float r = l * log2(w);
     return r;
 }
-
-
 /* --------------------------------------------------------------------- */
 int main(){
     int option;
     cout << "Super Hasheador-Inador" << endl;
-    cout << "Perry el ornitorrinco, quieres hashear? bueno, escoge tu arma!" << endl;
-    cout << "(1) Archivo" << endl;
-    cout << "(2) Input por consola" << endl;
-    cout << "(3) Calcular entropia" << endl;
+    cout << "Escoger lo que se desea hashear" << endl;
+    cout << "(1) Por Archivo" << endl;
+    cout << "(2) Por Input por consola" << endl;
+    cout << "(3) En caso de calcular entropia" << endl;
     cin >> option;
     if (option == 1){
         string archivo;
-        cout << "El camino dificil eh? pues dame el documento!" << endl;
+        cout << "Ingrese nombre del documento que se quiere hashear el contenido" << endl;
         cin >> archivo;
-        cout << "Estas loco? mira todo esto!" << endl;
-
-        //lectura del archivo
-
+        cout << "Output:" << endl;
         ifstream a;
         string line;
         a.open(archivo);
         a >> line;
         while (!a.eof()){
-            cout << line << endl;
+            string aux = lengthComp(line);
+            cout << "Texto: " << line << " ----> Post Hash : " << hasheador(aux) << endl;
             a >> line;
         }
-        
-        //hashear archivo
-        //hashArchivo(archivo);
-
-        cout << "Bueno, eso fue mucho trabajo, me voy a la cama. Adios Perry" << endl;
+        cout << "Proceso finalizado" << endl;
         return 0;
     }
     if (option == 2){
         string p;
-        cout << "El camino facil eh? pues dime lo que hashear!" << endl;
+        cout << "Ingrese string para hashear" << endl;
         cin >> p;
-        cout << "Anda, pues... aqui lo tienes!" << endl;
-
+        cout << "procesando..." << endl;
         //Comprobacion de largo
-        string r = lengthComp(p);
-
-        //hashear string
-        //hashString(r);
-
-        cout << "Eso fue facil eh?" << endl;
+        string a = lengthComp(p);
+        string r = hasheador(a);
+        cout << r << endl;
+        cout << "Proceso finalizado" << endl;
         return 0;
     }
     if (option == 3){
-        cout << "Ah, que quieres calcular la entropia, okey, pero, de que?" << endl;
+        cout << "Calculadora de entropia" << endl;
+        cout << "Ingrese opcion para calcular entropia:" << endl;
         int option2;
         cout << "(1) Archivo" << endl;
         cout << "(2) Input por consola" << endl;
         cin >> option2;
         if (option2 == 1){
-            /* calculo entropia de archivo */
+            string archivo;
+            cout << "Ingrese nombre del documento para calcular la entropia del contenido" << endl;
+            cin >> archivo;
+            cout << "Output:" << endl;
+            ifstream a;
+            string line;
+            a.open(archivo);
+            a >> line;
+            while (!a.eof()){
+                float aux = entropy(line);
+                cout << "Texto: " << line << " ----> entropia : " << aux << endl;
+                a >> line;
+            }
+            cout << "Proceso finalizado" << endl;
+            return 0;
         }
         if (option2 == 2){
-            /* calculo entropia de string */
-
-        }
-        
+            string in;
+            cout << "Ingrese el string para calcular la entropia del contenido" << endl;
+            cin>> in;
+            cout << "Output:" << endl;
+            float aux = entropy(in);
+            cout << "Texto: " << in << " ----> entropia : " << aux << endl;
+            cout << "Proceso finalizado" << endl;
+            return 0;
+        }   
     }
-    
     return 0;
 }
